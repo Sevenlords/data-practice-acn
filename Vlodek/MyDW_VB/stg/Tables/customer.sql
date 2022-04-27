b43d047a586cd16ca3e2ef7cad22c72058f54501
Volodymyr Bunin <volodymyr.bunin@accenture.com>
2022-04-27 10:56:58 +0200

    folder created

---
+++ Vlodek/MyDW_VB/stg/Tables/customer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [stg].[customer] (
+    [ModifiedDate]  DATETIME     NULL,
+    [CustomerID]    INT          NULL,
+    [PersonID]      INT          NULL,
+    [StoreID]       INT          NULL,
+    [TerritoryID]   INT          NULL,
+    [AccountNumber] VARCHAR (10) NULL,
+    [timeshtamp]    DATETIME     DEFAULT (getdate()) NULL
+);
+
