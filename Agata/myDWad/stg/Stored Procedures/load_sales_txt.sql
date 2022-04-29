b73f8bd665ac22ee6fd7fb9728b36b5ee132b5dc
dyraga <agatis101@gmail.com>
2022-04-29 18:45:02 +0200

    db

---
+++ Agata/myDWad/stg/Stored Procedures/load_sales_txt.sql
@@ -0,0 +1,27 @@
+﻿
+
+
+  CREATE proc [stg].[load_sales_txt]
+  as
+  begin
+
+  delete a
+  from stg.sales_txt a
+  join stg.sales_txt_delta b 
+  on a.order_number=b.order_number
+
+  insert into stg.sales_txt
+  select 
+	cast([order_number] as int)
+      ,cast([line_number] as int)
+      ,try_cast(date as datetime)
+      ,cast([reseller] as varchar(10))
+      ,[country]
+      ,cast([product] as varchar(25))
+      ,cast([qty] as smallint)
+      ,cast([unit_price] as money)
+      ,[timesthamp]
+      ,[filename]
+  from stg.sales_txt_delta
+
+  end
---
+++ Agata/myDWad/stg/Tables/sales_txt.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [stg].[sales_txt] (
+    [order_number] INT          NOT NULL,
+    [line_number]  INT          NULL,
+    [date]         DATETIME     NULL,
+    [reseller]     VARCHAR (10) NULL,
+    [country]      VARCHAR (50) NULL,
+    [product]      VARCHAR (25) NOT NULL,
+    [qty]          SMALLINT     NOT NULL,
+    [unit_price]   MONEY        NOT NULL,
+    [timesthamp]   DATETIME     DEFAULT (getdate()) NULL,
+    [filename]     VARCHAR (50) NULL
+);
+
