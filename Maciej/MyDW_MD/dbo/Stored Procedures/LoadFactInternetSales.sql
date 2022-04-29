db85de6f68fd52aa43ea2c74c596bc9f6028fe4b
MaDud1 <104552995+MaDud1@users.noreply.github.com>
2022-04-29 11:26:36 +0200

    Add files via upload

---
+++ Maciej/MyDW_MD/dbo/Stored Procedures/LoadFactInternetSales.sql
@@ -0,0 +1,45 @@
+﻿create procedure [dbo].[LoadFactInternetSales]
+as
+truncate table dbo.FactInternetSales
+
+insert into dbo.FactInternetSales
+([SalesOrderID],
+	[SalesOrderNumber],
+	[SalesOrderDetailID],
+	[DateKey],
+	[CustomerKey],
+	[ProductKey],
+	[OrderQuantity],
+	[UnitPrice],
+	[ExtendedAmount],
+	[UnitPriceDiscountPct],
+	[DiscountAmount],
+	[ProductStandardCost],
+	[TotalProductCost],
+	[SalesAmount])
+
+SELECT   
+	h.SalesOrderID as [SalesOrderID],
+	h.SalesOrderNumber as [SalesOrderNumber],
+	o.SalesOrderDetailID as [SalesOrderDetailID],
+	d.DateKey as [DateKey],
+	c.CustomerKey as [CustomerKey],
+	p.ProductKey as [ProductKey],
+	o.OrderQty as [OrderQuantity],
+	o.UnitPrice as [UnitPrice],
+	(o.OrderQty*o.UnitPrice) as [ExtendedAmount],
+	o.UnitPriceDiscount as [UnitPriceDiscountPct],
+	((o.OrderQty*o.UnitPrice)*o.UnitPriceDiscount) as [DiscountAmount],
+	p.Standardcost as [ProductStandardCost],
+	(p.Standardcost*o.OrderQty) as [TotalProductCost],
+	((o.OrderQty*o.UnitPrice)-((o.OrderQty*o.UnitPrice)*o.UnitPriceDiscount)) as [SalesAmount]
+	from
+	[stg].[Sales_SalesOrderHeader] as h
+	join [stg].Sales_SalesOrderDetail as o on h.SalesOrderID = o.SalesOrderID
+	join [MyDW].[dbo].DimProduct as p on p.ProductID = o.ProductID
+	join [MyDW].[dbo].DimCustomer c on c.CustomerID = h.CustomerID
+	join[MyDW].[dbo].DimDate as d on h.OrderDate = d.Date
+	where h.OnlineOrderFlag=1
+
+
+	exec LoadFactInternetSales
