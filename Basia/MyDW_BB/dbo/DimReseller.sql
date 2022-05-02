e30f5831542657e9a5c5ef997b293cdef266a72f
Barbara Brylka <barbara.brylka@accenture.com>
2022-05-02 11:18:16 +0200

    commit

---
+++ Basia/MyDW_BB/dbo/DimReseller.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DimReseller] (
+    [ResellerKey]   INT           IDENTITY (1, 1) NOT NULL,
+    [CustomerID]    INT           NOT NULL,
+    [AccountNumber] VARCHAR (10)  NULL,
+    [ResellerName]  NVARCHAR (50) NULL,
+    [CreatedDate]   DATETIME      DEFAULT (getdate()) NULL,
+    [ModifiedDate]  DATETIME      NULL
+);
+
