616fc5118311d67ecefd30cc1a7f15c0e2165a64
Volodymyr Bunin <volodymyr.bunin@accenture.com>
2022-05-05 15:39:59 +0200

    clean up

---
+++ Vlodek/MyDW_VB/dbo/Stored Procedures/LoadDimProduct_insert_update.sql
@@ -0,0 +1,106 @@
+﻿CREATE procedure [dbo].[LoadDimProduct_insert_update]
+as
+
+drop table if exists #product
+
+SELECT
+		P.ProductID AS [ProductID],
+		P.Name AS [ProductName],
+		P.ProductNumber AS [ProductAlternateKey],
+		P.StandardCost AS [StandardCost],
+		P.FinishedGoodsFlag AS [FinishedGoodsFlag],
+		ISNULL(P.Color, 'N/D') AS [Color],
+		P.ListPrice AS [ListPrice],
+		ISNULL(P.Size, 'N/D') AS [Size],
+		ISNULL(P.SizeUnitMeasureCode, 'N/D') AS [SizeUnitMeasureCode],
+		P.Weight AS [Weight],
+		ISNULL(P.WeightUnitMeasureCode, 'N/D') AS [WeightUnitMeasureCode],
+		P.DaysToManufacture AS [DaysToManufacture],
+		ISNULL(CONVERT(nchar(3),P.ProductLine), 'N/D') AS [ProductLine],
+		ISNULL(CONVERT(nchar(3),P.Class), 'N/D') AS [Class],
+		ISNULL(CONVERT(nchar(3),P.Style), 'N/D') AS [Style],
+		PS.ProductCategoryID AS [ProductCategoryID],
+		ISNULL(PC.Name, 'N/D') AS [ProductCategoryName],
+		P.ProductSubcategoryID AS [ProductSubcategoryID],
+		ISNULL(PS.Name, 'N/D')  AS [ProductSubcategoryName],
+		P.ProductModelID AS [ProductModelID],
+		ISNULL(PM.Name, 'N/D')  AS [ProductModelName],
+		P.SellStartDate AS [SellStartDate],
+		P.SellEndDate AS [SellEndDate],
+		P.ModifiedDate AS [SourceModifiedDate]
+into #product
+FROM [AdventureWorks2017].[Production].[Product] AS P
+		LEFT JOIN [AdventureWorks2017].[Production].[ProductSubcategory] AS PS
+		ON P.ProductSubcategoryID = PS.ProductSubcategoryID
+		LEFT JOIN [AdventureWorks2017].[Production].[ProductCategory] AS PC
+		ON PS.ProductCategoryID = PC.ProductCategoryID
+		LEFT JOIN [AdventureWorks2017].[Production].[ProductModel] AS PM
+		ON P.ProductModelID = PM.ProductModelID
+
+
+		
+update a
+set [ProductName] = b.[ProductName]
+      ,[ProductAlternateKey]=b.[ProductAlternateKey]
+      ,[StandardCost]=b.[StandardCost]
+      ,[FinishedGoodsFlag]=b.[FinishedGoodsFlag]
+      ,[Color]=b.[Color]
+      ,[ListPrice]=b.[ListPrice]
+      ,[Size]=b.[Size]
+      ,[SizeUnitMeasureCode]=b.[SizeUnitMeasureCode]
+      ,[Weight]=b.[Weight]
+      ,[WeightUnitMeasureCode]=b.[WeightUnitMeasureCode]
+      ,[DaysToManufacture]=b.[DaysToManufacture]
+      ,[ProductLine]=b.[ProductLine]
+      ,[Class]=b.[Class]
+      ,[Style]=b.[Style]
+      ,[ProductCategoryID]=b.[ProductCategoryID]
+      --,[ProductCategoryName]
+      --,[ProductSubcategoryID]
+      --,[ProductSubcategoryName]
+      --,[ProductModelID]
+      --,[ProductModelName]
+      --,[SellStartDate]
+      --,[SellEndDate]
+from DimProduct a 
+	join #product b ON a.ProductID = b.ProductID
+
+
+insert into [dbo].[DimProduct]
+(   [ProductID]
+      ,[ProductName]
+      ,[ProductAlternateKey]
+      ,[StandardCost]
+      ,[FinishedGoodsFlag]
+      ,[Color]
+      ,[ListPrice]
+      ,[Size]
+      ,[SizeUnitMeasureCode]
+      ,[Weight]
+      ,[WeightUnitMeasureCode]
+      ,[DaysToManufacture]
+      ,[ProductLine]
+      ,[Class]
+      ,[Style]
+      ,[ProductCategoryID]
+      ,[ProductCategoryName]
+      ,[ProductSubcategoryID]
+      ,[ProductSubcategoryName]
+      ,[ProductModelID]
+      ,[ProductModelName]
+      ,[SellStartDate]
+      ,[SellEndDate]
+      ,[SourceModifiedDate])
+
+SELECT a.*
+from #product a 
+	left join DimProduct b on a.ProductID = b.ProductID 
+where b.ProductID is null 
+
+
+
+--delete a
+---- select *
+--from DimProduct a 
+--	left join #product b on a.ProductID = b.ProductID
+--where b.ProductID is null
