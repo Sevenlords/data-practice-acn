5fdd72e2069fa3981817ef8c5e1d9a29ab3f0ef7
Barbara Brylka <barbara.brylka@accenture.com>
2022-05-05 10:16:48 +0200

    commit

---
+++ Basia/MyDW_BB/stg/Tables/sales_txt.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [stg].[sales_txt] (
+    [order_number] INT           NULL,
+    [line_number]  INT           NULL,
+    [date]         DATETIME      NULL,
+    [reseller]     NVARCHAR (30) NULL,
+    [country]      NVARCHAR (20) NULL,
+    [product]      NVARCHAR (30) NULL,
+    [qty]          INT           NULL,
+    [unit_price]   INT           NULL,
+    [timestamp]    DATETIME      DEFAULT (getdate()) NULL
+);
+
