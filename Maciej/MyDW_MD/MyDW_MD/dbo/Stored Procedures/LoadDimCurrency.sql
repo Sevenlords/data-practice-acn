1bc1252bdc5b93f9c4801effcfce201145e82dbc
MaDud1 <104552995+MaDud1@users.noreply.github.com>
2022-05-06 10:29:54 +0200

    upd

---
+++ Maciej/MyDW_MD/MyDW_MD/dbo/Stored Procedures/LoadDimCurrency.sql
@@ -0,0 +1,15 @@
+﻿create procedure [dbo].[LoadDimCurrency]
+as
+
+truncate table dbo.DimCurrency
+
+insert into dbo.DimCurrency
+([CurrencyAlternateKey]
+      ,[CurrencyName]
+      ,[SourceModifiedDate])
+
+SELECT   
+	CurrencyCode as [CurrencyAlternateKey],
+	Name as [CurrencyName],
+	ModifiedDate as [SourceModifiedDate]
+	from [AdventureWorks2017].[Sales].[Currency]
