5cf2b9f99c3476d9d60d555b912a42e78e47fcbe
MaDud1 <104552995+MaDud1@users.noreply.github.com>
2022-05-06 10:28:27 +0200

    del

--- Maciej/MyDW_MD/stg/Tables/Person_EmailAddress.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [stg].[Person_EmailAddress] (
-    [ModifiedDate]     DATETIME      NULL,
-    [BusinessEntityID] INT           NULL,
-    [EmailAddressID]   INT           NULL,
-    [EmailAddress]     NVARCHAR (50) NULL,
-    [timestamp]        DATETIME      DEFAULT (getdate()) NULL
-);
-
