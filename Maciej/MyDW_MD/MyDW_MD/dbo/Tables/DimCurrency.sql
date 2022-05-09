01bcec10e49010120b3a9324478c6b54c91243db
MaDud1 <104552995+MaDud1@users.noreply.github.com>
2022-05-09 09:13:22 +0200

    upd

---
+++ Maciej/MyDW_MD/MyDW_MD/dbo/Tables/DimCurrency.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DimCurrency] (
+    [CurrencyKey]          INT           IDENTITY (1, 1) NOT NULL,
+    [CurrencyAlternateKey] NCHAR (3)     NOT NULL,
+    [CurrencyName]         NVARCHAR (50) NOT NULL,
+    [SourceModifiedDate]   DATETIME      NOT NULL,
+    [CreatedDate]          DATETIME      DEFAULT (getdate()) NULL,
+    [ModifiedDate]         DATETIME      NULL
+);
+
---
+++ Maciej/MyDW_MD/MyDW_MD/dbo/Tables/DimDate.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[DimDate] (
+    [DateKey]      INT         NOT NULL,
+    [Date]         DATETIME    NULL,
+    [DayOfMonth]   VARCHAR (2) NULL,
+    [DayName]      VARCHAR (9) NULL,
+    [Month]        VARCHAR (2) NULL,
+    [MonthName]    VARCHAR (9) NULL,
+    [Quarter]      CHAR (1)    NULL,
+    [QuarterName]  VARCHAR (9) NULL,
+    [Year]         CHAR (4)    NULL,
+    [YearName]     CHAR (7)    NULL,
+    [MonthYear]    CHAR (10)   NULL,
+    [MMYYYY]       CHAR (6)    NULL,
+    [IsWeekday]    BIT         NULL,
+    [CreatedDate]  DATETIME    DEFAULT (getdate()) NULL,
+    [ModifiedDate] DATETIME    NULL,
+    PRIMARY KEY CLUSTERED ([DateKey] ASC)
+);
+
+
+
