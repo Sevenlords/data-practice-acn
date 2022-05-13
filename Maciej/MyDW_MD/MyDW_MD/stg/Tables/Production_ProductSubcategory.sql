f71f1e88cdaf5616b999a36aefc7fafaa1d2e08f
MaDud1 <104552995+MaDud1@users.noreply.github.com>
2022-05-13 09:03:34 +0200

    Delete Maciej/MyDW_MD directory
    upd

--- Maciej/MyDW_MD/MyDW_MD/stg/Tables/Production_ProductSubcategory.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [stg].[Production_ProductSubcategory] (
-    [ModifiedDate]         DATETIME      NULL,
-    [ProductSubcategoryID] INT           NULL,
-    [ProductCategoryID]    INT           NULL,
-    [Name]                 NVARCHAR (50) NULL,
-    [timestamp]            DATETIME      DEFAULT (getdate()) NULL
-);
-
--- Maciej/MyDW_MD/MyDW_MD/stg/Tables/Sales_Currency.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [stg].[Sales_Currency] (
-    [ModifiedDate] DATETIME      NULL,
-    [CurrencyCode] NVARCHAR (3)  NULL,
-    [Name]         NVARCHAR (50) NULL,
-    [timestamp]    DATETIME      DEFAULT (getdate()) NULL
-);
-
