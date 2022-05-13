1ff7f6ab24c72361b4748634bc8989717fc83cc1
MaDud1 <104552995+MaDud1@users.noreply.github.com>
2022-05-13 09:05:48 +0200

    sales_txt incr

---
+++ Maciej/MyDW_MD/MyDW_MD/dbo/Tables/FactResellerSales.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[FactResellerSales] (
+    [SalesOrderID]         INT           NOT NULL,
+    [SalesOrderNumber]     NVARCHAR (25) NOT NULL,
+    [SalesOrderDetailID]   INT           NOT NULL,
+    [DateKey]              INT           NOT NULL,
+    [ResellerKey]          INT           NOT NULL,
+    [ProductKey]           INT           NOT NULL,
+    [OrderQuantity]        SMALLINT      NOT NULL,
+    [UnitPrice]            MONEY         NOT NULL,
+    [ExtendedAmount]       INT           NOT NULL,
+    [UnitPriceDiscountPct] MONEY         NOT NULL,
+    [DiscountAmount]       MONEY         NOT NULL,
+    [ProductStandardCost]  MONEY         NOT NULL,
+    [TotalProductCost]     MONEY         NOT NULL,
+    [SalesAmount]          MONEY         NOT NULL,
+    [SourceID]             NVARCHAR (40) NOT NULL,
+    [CreatedDate]          DATETIME      DEFAULT (getdate()) NULL,
+    [ModifiedDate]         DATETIME      NULL
+);
+
+
+GO
---
+++ Maciej/MyDW_MD/MyDW_MD/stg/Tables/Person_EmailAddress.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [stg].[Person_EmailAddress] (
+    [ModifiedDate]     DATETIME      NULL,
+    [BusinessEntityID] INT           NULL,
+    [EmailAddressID]   INT           NULL,
+    [EmailAddress]     NVARCHAR (50) NULL,
+    [timestamp]        DATETIME      DEFAULT (getdate()) NULL
+);
+
