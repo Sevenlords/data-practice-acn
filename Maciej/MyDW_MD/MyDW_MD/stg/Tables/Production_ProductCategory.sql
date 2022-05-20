a45fe83de2baa232d590ac1728b5d24cef0aa267
MaDud1 <104552995+MaDud1@users.noreply.github.com>
2022-05-20 08:21:06 +0200

    change

--- Maciej/MyDW_MD/MyDW_MD/stg/Tables/Production_ProductCategory.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [stg].[Production_ProductCategory] (
-    [ModifiedDate]      DATETIME      NULL,
-    [Name]              NVARCHAR (50) NULL,
-    [ProductCategoryID] INT           NULL,
-    [timestamp]         DATETIME      DEFAULT (getdate()) NULL
-);
-
