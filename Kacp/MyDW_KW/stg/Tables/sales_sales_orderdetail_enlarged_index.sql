a82ab1ef0db24259cc9e52615dc52db72cd3ad87
Wozuka <woz_kacper@outlook.com>
2022-06-20 04:17:15 +0200

    add enlarged, index part 1

---
+++ Kacp/MyDW_KW/stg/Tables/sales_sales_orderdetail_enlarged_index.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [stg].[sales_sales_orderdetail_enlarged_index] (
+    [SalesOrderID]          INT              NOT NULL,
+    [SalesOrderDetailID]    INT              IDENTITY (1, 1) NOT NULL,
+    [CarrierTrackingNumber] NVARCHAR (25)    NULL,
+    [OrderQty]              SMALLINT         NOT NULL,
+    [ProductID]             INT              NOT NULL,
+    [SpecialOfferID]        INT              NOT NULL,
+    [UnitPrice]             MONEY            NOT NULL,
+    [UnitPriceDiscount]     MONEY            NOT NULL,
+    [LineTotal]             AS               (isnull(([UnitPrice]*((1.0)-[UnitPriceDiscount]))*[OrderQty],(0.0))),
+    [rowguid]               UNIQUEIDENTIFIER ROWGUIDCOL NOT NULL,
+    [ModifiedDate]          DATETIME         NOT NULL,
+    CONSTRAINT [PK_SalesOrderDetailEnlarged_SalesOrderID_SalesOrderDetailID] PRIMARY KEY CLUSTERED ([SalesOrderID] ASC, [SalesOrderDetailID] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_SalesOrderDetailEnlarged_ProductID]
+    ON [stg].[sales_sales_orderdetail_enlarged_index]([ProductID] ASC);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [AK_SalesOrderDetailEnlarged_rowguid]
+    ON [stg].[sales_sales_orderdetail_enlarged_index]([rowguid] ASC);
+
